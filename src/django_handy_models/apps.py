461fa09a9d803daab33a9bd24b2f647daa3d11ab
Felix Eklöf <felix.eklof@tinfonder.se>
2021-05-05 13:54:46 +0200

    initial commit

--- src/django_handy_models/apps.py
+++ src/django_handy_models/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoUsefulModelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_useful_models'
+    name = 'django_handy_models'
