8f4a55eb4698b1e3d4f62eb289be704032c31807
Felix Eklöf <felix.eklof@tinfonder.se>
2021-05-05 15:09:44 +0200

    remove extra data from fixtures

--- src/django_handy_models/admin.py
+++ src/django_handy_models/admin.py
@@ -3,9 +3,7 @@ from django.conf import settings
 
 from .models import Country, Currency, Continent, Language, Timezone
 
-print(getattr(settings, 'USEFUL_MODELS_REGISTER_IN_ADMIN', False))
-
-if getattr(settings, 'USEFUL_MODELS_REGISTER_IN_ADMIN', False) is True:
+if getattr(settings, 'HANDY_MODELS_REGISTER_IN_ADMIN', False) is True:
     admin.site.register(Country)
     admin.site.register(Currency)
     admin.site.register(Continent)
--- src/django_handy_models/apps.py
+++ src/django_handy_models/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DjangoUsefulModelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'django_handy_models'
+    verbose_name = 'Django Handy Models'
